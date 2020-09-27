5df3a25fa348090185fba26e9ff3a448526d30f5
Samyu Miller <amille49@lesley.edu>
2020-09-26 16:11:24 -1000

    change index.html to index php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+        <li><a href="./index.html">HOME</a></li>
+        <!-- <li><a href="./post.html">post</a></li> -->
+        <li><a href="./post-submission.html">CREATE POST</a></li>
+    </ul>
+    </nav> 
+    <header class="center">
+        Samyu Miller's Blog
+    </header>
+    <div class="center-blog-post">
+        <p>BLOG POSTS</p>
+    </div>
+    <main>
+        
+        <li><a href="./post.html"><div class="post-card">Post #1</div></a></li>
+    
+    </main>
+    <footer>
+        Follow Me 
+    </footer>
+</body>
+</html>
